de83e12655533059da0961a5aa7d9d87426c7c67
Eugene <clackd1amond@users.noreply.github.com>
2021-06-18 11:13:24 +0300

    answers list

---
+++ src/components/ActiveQuiz/AnswersList/AnswersList.js
@@ -0,0 +1,12 @@
+import classes from './AnswersList.module.css';
+import AnswerItem from './AnswerItem/AnswerItem';
+
+const AnswersList = (props) => (
+	<ul className={classes.AnswersList}>
+		{props.answers.map((answer, index) => {
+			return <AnswerItem key={index} answer={answer} />;
+		})}
+	</ul>
+);
+
+export default AnswersList;
