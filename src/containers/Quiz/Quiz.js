aebcb3ed91824ee51990add4b752e8f6a28264fc
Eugene <clackd1amond@users.noreply.github.com>
2021-06-18 13:41:25 +0300

    show next question

--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -5,8 +5,10 @@ import ActiveQuiz from '../../components/ActiveQuiz/ActiveQuiz';
 export const AnswerContext = React.createContext();
 class Quiz extends Component {
 	state = {
+		activeQuestion: 0,
 		quiz: [
 			{
+				id: 1,
 				question: 'Какого цвета небо?',
 				rightAnswerId: 2,
 				answers: [
@@ -16,11 +18,25 @@ class Quiz extends Component {
 					{ text: 'Зеленый', id: 4 },
 				],
 			},
+			{
+				id: 2,
+				question: 'В каком году основали Санкт-Петербург?',
+				rightAnswerId: 3,
+				answers: [
+					{ text: '1700', id: 1 },
+					{ text: '1702', id: 2 },
+					{ text: '1703', id: 3 },
+					{ text: '1803', id: 4 },
+				],
+			},
 		],
 	};
 
 	onAnswerClickHandler = (answerId) => {
 		console.log('Answer id: ', answerId);
+		this.setState({
+			activeQuestion: this.state.activeQuestion + 1,
+		});
 	};
 
 	render() {
@@ -29,7 +45,12 @@ class Quiz extends Component {
 				<div className={classes.QuizWrapper}>
 					<h1>Ответьте на все вопросы</h1>
 					<AnswerContext.Provider value={this.onAnswerClickHandler}>
-						<ActiveQuiz answers={this.state.quiz[0].answers} question={this.state.quiz[0].question} />
+						<ActiveQuiz
+							answers={this.state.quiz[this.state.activeQuestion].answers}
+							question={this.state.quiz[this.state.activeQuestion].question}
+							quizLength={this.state.quiz.length}
+							answerNumber={this.state.activeQuestion + 1}
+						/>
 					</AnswerContext.Provider>
 				</div>
 			</div>
