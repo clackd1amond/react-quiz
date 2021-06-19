51a40ca08ea58d3625f3dcdf3917a469d72a9fc3
Eugene <clackd1amond@users.noreply.github.com>
2021-06-19 18:18:37 +0300

    backdrop

--- src/components/Navigation/Drawer/Drawer.js
+++ src/components/Navigation/Drawer/Drawer.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import classes from './Drawer.module.css';
+import Backdrop from '../../UI/Backdrop/Backdrop';
 
 const links = [1, 2, 3];
 
@@ -18,9 +19,12 @@ class Drawer extends Component {
 		if (!this.props.isOpen) cls.push(classes.close);
 
 		return (
-			<nav className={cls.join(' ')}>
-				<ul>{this.renderLinks()}</ul>
-			</nav>
+			<React.Fragment>
+				<nav className={cls.join(' ')}>
+					<ul>{this.renderLinks()}</ul>
+				</nav>
+				{this.props.isOpen ? <Backdrop onClick={this.props.onClose} /> : null}
+			</React.Fragment>
 		);
 	}
 }
