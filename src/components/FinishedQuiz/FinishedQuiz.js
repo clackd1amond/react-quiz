d8a2eaf932e2071df234239e02209a01de552fea
Eugene <clackd1amond@users.noreply.github.com>
2021-06-21 12:33:38 +0300

    link router

--- src/components/FinishedQuiz/FinishedQuiz.js
+++ src/components/FinishedQuiz/FinishedQuiz.js
@@ -1,5 +1,6 @@
 import classes from './FinishedQuiz.module.css';
 import Button from '../UI/Button/Button';
+import { Link } from 'react-router-dom';
 
 const FinishedQuiz = (props) => {
 	const successCount = Object.keys(props.results).reduce((total, key) => {
@@ -36,7 +37,9 @@ const FinishedQuiz = (props) => {
 				<Button onClick={props.onRetry} type='primary'>
 					Повторить
 				</Button>
-				<Button type='success'>Перейти в список тестов</Button>
+				<Link to='/'>
+					<Button type='success'>Перейти в список тестов</Button>
+				</Link>
 			</div>
 		</div>
 	);
