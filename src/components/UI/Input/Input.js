a3f9cdc72e95c4a1b50ec0513ffbabf8c244482b
Eugene <clackd1amond@users.noreply.github.com>
2021-06-21 13:44:08 +0300

    input component & validation

---
+++ src/components/UI/Input/Input.js
@@ -0,0 +1,26 @@
+import classes from './Input.module.css';
+
+function isInvalid({ valid, touched, shouldValidate }) {
+	return !valid && shouldValidate && touched;
+}
+
+const Input = (props) => {
+	const inputType = props.type || 'text';
+	const cls = [classes.Input];
+	const htmlFor = `${inputType}-${Math.round(Math.random() * 1000000)}`;
+
+	if (isInvalid(props)) {
+		cls.push(classes.invalid);
+	}
+
+	return (
+		<div className={cls.join(' ')}>
+			<label htmlFor={htmlFor}>{props.label}</label>
+			<input type={inputType} id={htmlFor} value={props.value} onChange={props.onChange} />
+
+			{isInvalid(props) ? <span>{props.errorMessage || 'Введите верное значение'}</span> : null}
+		</div>
+	);
+};
+
+export default Input;
