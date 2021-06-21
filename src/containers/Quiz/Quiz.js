f14431548decde06ce86d6e69b62dd16994c79eb
Eugene <clackd1amond@users.noreply.github.com>
2021-06-21 12:30:28 +0300

    quiz list style

--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -80,6 +80,10 @@ class Quiz extends Component {
 		this.setState({ activeQuestion: 0, answerState: null, isFinished: false, results: {} });
 	};
 
+	componentDidMount() {
+		console.log('Quiz ID = ', this.props.match.params.id);
+	}
+
 	render() {
 		return (
 			<div className={classes.Quiz}>
