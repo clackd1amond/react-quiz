dbc5c440a3e06323282043eeda87b0c42cb65011
Eugene <clackd1amond@users.noreply.github.com>
2021-06-24 15:31:40 +0300

    auto login

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -2,10 +2,11 @@ import React, { Component } from 'react';
 import classes from './Layout.module.css';
 import MenuToggle from '../../components/Navigation/MenuToggle/MenuToggle';
 import Drawer from '../../components/Navigation/Drawer/Drawer';
+import { connect } from 'react-redux';
 
 class Layout extends Component {
 	state = {
-		menu: false,
+		menu: false
 	};
 
 	toggleMenuHandler = () => this.setState({ menu: !this.state.menu });
@@ -14,7 +15,11 @@ class Layout extends Component {
 	render() {
 		return (
 			<div className={classes.Layout}>
-				<Drawer isOpen={this.state.menu} onClose={this.menuCloseHandler} />
+				<Drawer
+					isOpen={this.state.menu}
+					onClose={this.menuCloseHandler}
+					isAuthenticated={this.props.isAuthenticated}
+				/>
 				<MenuToggle onToggle={this.toggleMenuHandler} isOpen={this.state.menu} />
 				<main>{this.props.children}</main>
 			</div>
@@ -22,4 +27,10 @@ class Layout extends Component {
 	}
 }
 
-export default Layout;
+function mapStateToProps(state) {
+	return {
+		isAuthenticated: !!state.auth.token
+	};
+}
+
+export default connect(mapStateToProps)(Layout);
